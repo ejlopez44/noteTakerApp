31ae5dc71ced7b3346ef6108517298391f7e8051
Tito <ejlopez44@gmail.com>
2020-05-10 16:26:44 -0400

    pushing files to heroku

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,4 +11,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
 };
